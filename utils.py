3a5c706ef8556bce0919e7c75266808449f5f060
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2025-04-10 11:42:21 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -211,8 +211,8 @@ async def get_shortlink(link, grp_id, is_second_shortener=False, is_third_shorte
         if is_third_shortener:             
             api, site = settings['api_three'], settings['shortner_three']
         elif:
-            if is_second_shortener:
-                api, site = settings['api_two'], settings['shortner_two']
+         if is_second_shortener:
+          api, site = settings['api_two'], settings['shortner_two']
              else:
                if is_four_shortener:
                    api, site = settings['api_four'], settings['shortner_four']
